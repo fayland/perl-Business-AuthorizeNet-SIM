c2739ed253fc6844d4434a95b1c575528c523c38
Fayland Lam <fayland@gmail.com>
2013-11-15 22:16:02 +0800

    basically working, without example

--- lib/Business/AuthorizeNet/SIM.pm
+++ lib/Business/AuthorizeNet/SIM.pm
@@ -3,6 +3,41 @@ package Business::AuthorizeNet::SIM;
 use strict;
 use 5.008_005;
 our $VERSION = '0.01';
+use Carp 'croak';
+use Digest::MD5  qw(md5 md5_hex);
+use Digest::HMAC qw(hmac);
+
+sub new {
+	my $class = shift;
+	my %args = (@_ % 2) ? %{$_[0]} : @_;
+
+	$args{api_login_id} or croak "api_login_id is required.";
+
+	return bless \%args, $class;
+}
+
+sub isAuthorizeNet {
+	my ($self) = @_;
+
+	return ($self->{md5_hash} && ($self->generateHash() eq $self->{md5_hash}));
+}
+
+sub generateHash {
+	my ($self) = @_;
+
+	my $amount = $self->{amount} ? $self->{amount} : '0.00';
+	return uc(md5_hex($self->{md5_settings}, $self->{api_login_id}, $self->{transaction_id} . $self->{amount}));
+}
+
+sub getFingerprint {
+	my ($self, $api_login_id, $transaction_key, $amount, $fp_sequence, $fp_timestamp) = @_;
+
+	return hmac_md5_hex($api_login_id . "^" . $fp_sequence . "^" . $fp_timestamp . "^" . $amount . "^", $transaction_key);
+}
+
+sub hmac_md5_hex {
+    unpack("H*", hmac($_[0], $_[1], \&md5, 64))
+}
 
 1;
 __END__
@@ -11,7 +46,7 @@ __END__
 
 =head1 NAME
 
-Business::AuthorizeNet::SIM - Blah blah blah
+Business::AuthorizeNet::SIM - AUthorize.Net SIM
 
 =head1 SYNOPSIS
 
--- t/basic.t
+++ t/basic.t
@@ -2,7 +2,32 @@ use strict;
 use Test::More;
 use Business::AuthorizeNet::SIM;
 
-# replace with the actual test
-ok 1;
+my $sim = Business::AuthorizeNet::SIM->new(
+	amount => '4.12',
+	transaction_id => '123',
+	api_login_id => '528udYYwz',
+	md5_settings => 'test',
+);
+is($sim->generateHash(), '8FC33C32ABB3EDD8BBC4BE3E904CB47E');
+
+$sim = Business::AuthorizeNet::SIM->new(
+	amount => '4.12',
+	transaction_id => '123',
+	api_login_id => '528udYYwz',
+	md5_settings => 'test',
+	md5_hash => '8FC33C32ABB3EDD8BBC4BE3E904CB47E',
+);
+ok($sim->isAuthorizeNet());
+
+$sim = Business::AuthorizeNet::SIM->new(
+	amount => '4.12',
+	transaction_id => '123',
+	api_login_id => '528udYYwz',
+	md5_settings => 'test',
+	md5_hash => '8FC33C32BB3EDD8BBC4BE3E904CB47E',
+);
+ok(! $sim->isAuthorizeNet());
+
+is( $sim->getFingerprint("123","123","123","123","123"), 'db88bbebb8f699acdbe70daad897a68a' );
 
 done_testing;
